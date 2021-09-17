5f0d640bdee5de83d4aaadd1639d996a3da4166b
Daniel <itzdanielmichael@gmail.com>
2021-09-16 21:04:14 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { csv } from 'd3-fetch';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import './App.css';
 import TeamsCSV from './Teams.csv';
 import BattingCSV from './Batting.csv';
+import { useTable, usePagination } from 'react-table';
 
 // components
 import Table from '@mui/material/Table';
@@ -13,10 +14,91 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+
+// icons
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
+// utils
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  tableRoot: {
+    width: '100%',
+  },
+}));
 
 function App() {
   const [teams, setTeams] = useState([]); // use teams to get teamname
   const [players, setPlayers] = useState([]); // each individual players data, use teams to get the teamname
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'playerId',
+        accessor: 'playerId',
+        Cell: ({ cell }) => {
+          const playerData = cell.row.original;
+
+          return <span>{playerData.playerID}</span>;
+        },
+      },
+      {
+        Header: 'yearId',
+        accessor: 'yearId',
+        Cell: ({ cell }) => {
+          const playerData = cell.row.original;
+          return <span>{playerData.yearID}</span>;
+        },
+      },
+      // {
+      //   Header: 'Team name(s)',
+      //   accessor: 'teamName',
+      //   Cell: (props) => {
+      //     console.log({ props });
+      //     return <span>teamName Cell</span>;
+      //   },
+      // },
+
+      {
+        Header: 'Batting Average',
+        accessor: 'battingAverage',
+        Cell: (props) => {
+          console.log({ props });
+          return <span>battingAverage Cell</span>;
+        },
+      },
+    ],
+    // check for these dependencies changing to rerender the data.
+    [players]
+  );
+
+  const {
+    gotoPage,
+    setPageSize,
+    canPreviousPage,
+    pageOptions,
+    canNextPage,
+    state: { pageIndex, pageSize },
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page: rows, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data: players,
+      initialState: { pageIndex: 0, pageSize: 20 },
+    },
+    usePagination
+  ); // react-table hooks
+
+  const classes = useStyles();
 
   useEffect(() => {
     csv(TeamsCSV).then((data) => {
@@ -32,22 +114,48 @@ function App() {
     });
   }, []);
 
-  const COLUMNS = ['playerID', 'yearId', 'Team name(s)', 'Batting Average'];
-
   return (
     <div className="App">
-      <table>
-        <thead>
-          {COLUMNS.map((columnText) => (
-            <th>{columnText}</th>
-          ))}
-        </thead>
-        <tr>
-          {/* {players.map((player) => {
-            return <>text</>;
-          })} */}
-        </tr>
-      </table>
+      <TableContainer className={classes.tableContainer}>
+        <Table stickyHeader className={classes.table} {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map((headerGroup) => (
+              // table headers
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <TableCell
+                      {...column.getHeaderProps()}
+                      className={classes.tableHeader}>
+                      {column.render('Header')}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHead>
+
+          <TableBody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              console.log({ row });
+              prepareRow(row);
+              // table rows
+              return (
+                <TableRow {...row.getRowProps()} hover key={i}>
+                  {row.cells.map((cell, i) => {
+                    return (
+                      // cell of row.
+                      <TableCell key={i} {...cell.getCellProps()}>
+                        {cell.render('Cell')}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
