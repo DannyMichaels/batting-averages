1c44c77dd5af93ccb176eb08a29da77fe7e69753
Daniel <itzdanielmichael@gmail.com>
2021-09-16 21:29:45 -0400

    update index.css and App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import TableRow from '@mui/material/TableRow';
 import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
 import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 // icons
 import FirstPageIcon from '@mui/icons-material/FirstPage';
@@ -28,6 +30,22 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles((theme) => ({
   tableRoot: {
     width: '100%',
+  },
+  tableContainer: {
+    maxHeight: 500,
+  },
+  paginationSpacer: {
+    flex: '1 1 100%',
+    [theme.breakpoints.down('md')]: {
+      flex: '0 0',
+    },
+  },
+  paginationActions: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+    [theme.breakpoints.down('sm')]: {
+      flexShrink: 1, // direction buttons in a column in small screen
+    },
   },
 }));
 
@@ -53,6 +71,7 @@ function App() {
   const [teams, setTeams] = useState([]); // use teams to get teamname
   const [players, setPlayers] = useState([]); // each individual players data, use teams to get the teamname
 
+  console.log({ teams });
   const columns = useMemo(
     () => [
       {
@@ -72,15 +91,20 @@ function App() {
           return <span>{playerData.yearID}</span>;
         },
       },
-      // {
-      //   Header: 'Team name(s)',
-      //   accessor: 'teamName',
-      //   Cell: (props) => {
-      //     console.log({ props });
-      //     return <span>teamName Cell</span>;
-      //   },
-      // },
+      {
+        Header: 'Team name(s)',
+        accessor: 'teamName',
+        Cell: ({ cell }) => {
+          const playerData = cell.row.original;
+          // find the team of the player by teamId
+          // return the team's name
+          let team = [...teams].filter(
+            ({ teamID }) => teamID === playerData.teamID
+          );
 
+          return <span>{team.name}</span>;
+        },
+      },
       {
         Header: 'Batting Average',
         accessor: 'battingAverage',
@@ -91,8 +115,8 @@ function App() {
         },
       },
     ],
-    // check for these dependencies changing to rerender the data.
-    [players]
+    // check for these dependencies changing to trigger a variable change.
+    [players, teams]
   );
 
   const {
@@ -121,61 +145,137 @@ function App() {
 
   useEffect(() => {
     csv(TeamsCSV).then((data) => {
-      console.log('teams.scsv', data);
-      let dataWithoutColumns = data.slice(0, -1);
+      let dataWithoutColumns = data.slice(0, -1); // get rid of the columns object in the array.
       setTeams(dataWithoutColumns);
     });
 
     csv(BattingCSV).then((data) => {
-      console.log('batting.csv', data);
-      let dataWithoutColumns = data.slice(0, -1);
+      let dataWithoutColumns = data.slice(0, -1); // get rid of the columns object in the array.
       setPlayers(dataWithoutColumns);
     });
   }, []);
 
   return (
-    <div className="App">
-      <TableContainer className={classes.tableContainer}>
-        <Table stickyHeader className={classes.table} {...getTableProps()}>
-          <TableHead>
-            {headerGroups.map((headerGroup) => (
-              // table headers
-              <TableRow {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => {
-                  return (
-                    <TableCell
-                      {...column.getHeaderProps()}
-                      className={classes.tableHeader}>
-                      {column.render('Header')}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHead>
-
-          <TableBody {...getTableBodyProps()}>
-            {rows.map((row, i) => {
-              console.log({ row });
-              prepareRow(row);
-              // table rows
-              return (
-                <TableRow {...row.getRowProps()} hover key={i}>
-                  {row.cells.map((cell, i) => {
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh">
+      <Paper>
+        <TableContainer className={classes.tableContainer}>
+          <Table stickyHeader className={classes.table} {...getTableProps()}>
+            <TableHead>
+              {headerGroups.map((headerGroup) => (
+                // table headers
+                <TableRow {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => {
                     return (
-                      // cell of row.
-                      <TableCell key={i} {...cell.getCellProps()}>
-                        {cell.render('Cell')}
+                      <TableCell
+                        {...column.getHeaderProps()}
+                        className={classes.tableHeader}>
+                        {column.render('Header')}
                       </TableCell>
                     );
                   })}
                 </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+              ))}
+            </TableHead>
+
+            <TableBody {...getTableBodyProps()}>
+              {rows.map((row, i) => {
+                prepareRow(row);
+                // table rows
+                return (
+                  <TableRow {...row.getRowProps()} hover key={i}>
+                    {row.cells.map((cell, i) => {
+                      return (
+                        // cell of row.
+                        <TableCell key={i} {...cell.getCellProps()}>
+                          {cell.render('Cell')}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <TableFooter>
+          <TableRow>
+            {/* TABLE PAGINATION */}
+
+            {/* Material UI TablePagination component */}
+            <TablePagination
+              rowsPerPageOptions={[10, 20, 30, 40, 50]}
+              colSpan={0}
+              className={classes.pagination}
+              count={pageOptions.length}
+              rowsPerPage={pageSize}
+              onPageChange={gotoPage}
+              page={pageIndex}
+              classes={{ spacer: classes.paginationSpacer }}
+              onRowsPerPageChange={(e) => setPageSize(Number(e.target.value))}
+              ActionsComponent={(props) => {
+                const { count, page, rowsPerPage, onPageChange } = props;
+
+                const handleFirstPageButtonClick = () => {
+                  onPageChange(0);
+                };
+
+                const handleBackButtonClick = () => {
+                  if (!canPreviousPage) return;
+
+                  const previousPage = page - 1;
+                  onPageChange(previousPage);
+                };
+
+                const handleNextButtonClick = () => {
+                  if (!canNextPage) return;
+
+                  const nextPage = page + 1;
+                  onPageChange(nextPage);
+                };
+
+                const handleLastPageButtonClick = () => {
+                  onPageChange(Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+                };
+
+                return (
+                  <div id="ACTIONS" className={classes.paginationActions}>
+                    <IconButton
+                      onClick={handleFirstPageButtonClick}
+                      disabled={page === 0}
+                      aria-label="first page">
+                      <FirstPageIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={handleBackButtonClick}
+                      disabled={page === 0}
+                      aria-label="previous page">
+                      <KeyboardArrowLeft />
+                    </IconButton>
+                    <IconButton
+                      onClick={handleNextButtonClick}
+                      disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                      aria-label="next page">
+                      <KeyboardArrowRight />
+                    </IconButton>
+                    <IconButton
+                      onClick={handleLastPageButtonClick}
+                      disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                      aria-label="last page">
+                      <LastPageIcon />
+                    </IconButton>
+                  </div>
+                );
+              }}
+            />
+          </TableRow>
+        </TableFooter>
+      </Paper>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@mui/styles/';
+import { createTheme } from '@mui/material/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
